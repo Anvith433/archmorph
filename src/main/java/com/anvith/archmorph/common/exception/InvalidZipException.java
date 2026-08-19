42cfadf0d543046347dc79b0419b36fb128fcbb7
Anvith433 <anvithshetty932@gmail.com>
2026-08-19 12:39:42 +0100

    feat: handle invalid ZIP archives

---
+++ src/main/java/com/anvith/archmorph/common/exception/InvalidZipException.java
@@ -0,0 +1,10 @@
+package com.anvith.archmorph.common.exception;
+
+public class InvalidZipException extends RuntimeException {
+
+    public InvalidZipException(String message) 
+    {
+        super(message);
+    }
+
+}
