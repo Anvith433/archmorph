7dbdf8b8a2bb37c4c7cfd907c8259d2de77c8ea3
Anvith433 <anvithshetty932@gmail.com>
2026-08-19 12:51:42 +0100

    feat: model architecture diagram results

---
+++ src/main/java/com/anvith/archmorph/analysis/diagram/DiagramReport.java
@@ -0,0 +1,21 @@
+package com.anvith.archmorph.analysis.diagram;
+
+/**
+ * Stores generated diagram.
+ */
+public class DiagramReport {
+
+    /*
+     * PlantUML text.
+     */
+    private String plantUml;
+
+    public String getPlantUml() {
+        return plantUml;
+    }
+
+    public void setPlantUml(String plantUml) {
+        this.plantUml = plantUml;
+    }
+
+}
