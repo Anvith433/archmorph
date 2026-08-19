9e201ad85e5d205c4cd3f60c69d2577e6f729699
Anvith433 <anvithshetty932@gmail.com>
2026-08-19 12:39:16 +0100

    feat: define workspace directory constants

---
+++ src/main/java/com/anvith/archmorph/common/constants/WorkspaceConstants.java
@@ -0,0 +1,18 @@
+package com.anvith.archmorph.common.constants;
+public final class WorkspaceConstants {
+    private WorkspaceConstants() {
+    }
+    public static final String ARCHIVES = "archives";
+    public static final String PROJECTS = "projects";
+
+    public static final String ORIGINAL = "original";
+    public static final String WORKING = "working";
+    public static final String ANALYSIS = "analysis";
+    public static final String OUTPUT = "output";
+    public static final String REPORTS = "reports";
+
+    public static final String TEMP = "temp";
+    public static final String LOGS = "logs";
+    public static final String CACHE = "cache";
+
+}
