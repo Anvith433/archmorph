d9e367ab565c057060302b27ed60baa188daf63b
Anvith433 <anvithshetty932@gmail.com>
2026-08-19 12:55:20 +0100

    test: verify Spring Boot application context

---
+++ src/test/java/com/anvith/archmorph/ArchmorphAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anvith.archmorph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchmorphAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
